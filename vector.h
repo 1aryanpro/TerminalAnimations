b8dc4f1e3b4d0e9d23278b8c04de99e733c6a3e0
smashMineGame <aryanp@khanlabschool.org>
2021-05-31 11:55:37 -0700

    Added Arithmatic Vector Methods

--- vector.h
+++ vector.h
@@ -2,27 +2,52 @@
 #define VECTOR_H
 
 typedef struct {
-	int x;
-	int y;
+	float x;
+	float y;
 } Vector;
 
 namespace vector
 {
-	Vector *make(int x, int y)
+	Vector make(float x, float y)
 	{
-		Vector* out = (Vector*)malloc(sizeof(Vector));
-		out->x = x;
-		out->y = y;
+		Vector out;
+		out.x = x;
+		out.y = y;
 
 		return out;
 	}
 
-	int cmp(Vector *a, Vector *b)
+	int cmp(Vector a, Vector b)
 	{
-		if (a->x != b->x) { return 0; }
-		if (a->y != b->y) { return 0; }
+		if (a.x != b.x) { return 0; }
+		if (a.y != b.y) { return 0; }
 		return 1;
 	}
+
+	Vector add(Vector a, Vector b)
+	{
+		Vector out;
+		out.x = a.x + b.x;
+		out.y = a.y + b.y;
+		return out;
+	}
+
+	Vector mul(Vector a, Vector b)
+	{
+		Vector out;
+		out.x = a.x * b.x;
+		out.y = a.y * b.y;
+		return out;
+	}
+
+	Vector mul(Vector a, float s)
+	{
+		Vector out;
+		out.x = a.x * s;
+		out.y = a.y * s;
+		return out;
+	}
+
 }
 
 #endif
